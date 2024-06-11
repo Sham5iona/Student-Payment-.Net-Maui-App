9572a98fd80c07670453dbbebd08cd0f09d9a3a3
Sham5iona <gom26@abv.bg>
2024-06-11 18:42:29 +0300

    This are the models of the database! Student is for responsible to store any information for the child includely parent name! The Payment one holds the data about the payment context!

---
+++ DatabasePaymentManagementWEBAPI/Model/Payment.cs
@@ -0,0 +1,39 @@
+﻿namespace DatabasePaymentManagementWEBAPI.Model
+{
+    public class Payment
+    {
+        private int _id;
+        public int Id { get { return _id; } set { _id = value; } }
+
+        private int _student_id;
+        public int StudentId { get { return _student_id; } set { _student_id = value; } }
+        public Student Student { get; set; }
+
+        private decimal _givenAmount;
+        public decimal GivenAmount { get { return _givenAmount; } 
+                                     set { _givenAmount = value; } }
+
+        private decimal _lastAmount;
+        public decimal LastAmount { get { return _lastAmount; } 
+                                    set { _lastAmount = value; } }
+
+        private DateTime _lastModification;
+        public DateTime LastModification
+        {
+            get { return _lastModification; }
+            set { _lastModification = DateTime.Now; }
+        }
+
+        public Payment(int studentId, decimal givenAmount, decimal lastAmount)
+        {
+            this.StudentId = studentId;
+            this.GivenAmount = givenAmount;
+            this.LastAmount = lastAmount;
+        }
+
+        public Payment() //Empty constructor for EFCore
+        {
+            
+        }
+    }
+}
---
+++ DatabasePaymentManagementWEBAPI/Model/Student.cs
@@ -0,0 +1,44 @@
+﻿namespace DatabasePaymentManagementWEBAPI.Model
+{
+    public class Student
+    {
+        private int _id;
+        public int Id { get { return _id; } set { _id = value; } }
+
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        private int _age;
+        public int Age { get { return _age; } set { _age = value; } }
+
+        private string _location;
+        public string Location { get { return _location; } set { _location = value; } }
+
+        private string? _parentName;
+        public string? ParentName { get { return _parentName;} set { _parentName = value; } }
+
+        private bool _isActive;
+        public bool IsActive { get { return _isActive; } set { _isActive = value; } }
+
+        public Payment Payment { get; set; }
+
+        private DateTime _lastModification;
+        public DateTime LastModification { get { return _lastModification; } 
+                                           set { _lastModification = DateTime.Now; } }
+
+        public Student(string name, int age, string location, string? parentName,
+                        bool isActive)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Location = location;
+            this.ParentName = parentName;
+            this.IsActive = isActive;
+        }
+
+        public Student() //Empty Constructor for EF Core
+        {
+            
+        }
+    }
+}
