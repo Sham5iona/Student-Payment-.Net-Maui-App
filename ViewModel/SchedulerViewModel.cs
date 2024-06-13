582d0577fddedd0273326bb6e9f4941fe425416c
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-06-13 16:42:50 +0300

    ViewModel
    This is the first view model which is responsible for the scheduler page! It will define the appointments and the observable properties of the inputs!

---
+++ ViewModel/SchedulerViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.Maui.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentPaymentApp.ViewModel
+{
+    public class SchedulerViewModel
+    {
+        public ObservableCollection<SchedulerAppointment> Events { get; set; }
+
+        public SchedulerViewModel()
+        {
+            Events = new ObservableCollection<SchedulerAppointment>
+            {
+                new SchedulerAppointment
+                {
+                     StartTime = new DateTime(2024, 6, 5, 7, 0, 0),
+                     EndTime = new DateTime(2024, 6, 5, 14, 30, 0),
+                     Subject="Hunting",
+                     Background=Colors.Blue,
+                     Location="Шуманица"
+                }
+            };
+
+        }
+    }
+}
