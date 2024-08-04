1b430f420f3bba88916610d8ea95d54d1af40a15
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-08-04 14:35:49 +0300

    Db Context
    Here I have added the intercations with the database for the students like for CRUD and ordering asc and desc.

---
+++ StudentPaymentDbContext.cs
@@ -0,0 +1,107 @@
+﻿using SQLite;
+using StudentPaymentApp.Model;
+
+namespace StudentPaymentApp.Data
+{
+    public class StudentPaymentDbContext
+    {
+        private const string DB_NAME = "StudentPaymentDb.db3";
+        private SQLiteAsyncConnection _connection;
+
+        public StudentPaymentDbContext()
+        {
+            Init();
+        }
+        private async void Init()
+        {
+            _connection = new SQLiteAsyncConnection(
+                              Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+            if (_connection != null)
+            {
+                return;
+            }
+
+            await _connection.CreateTableAsync<Student>();
+            await _connection.CreateTableAsync<Appointment>();
+        }
+
+        public async Task AddStudentAsync(Student student)
+        {
+            int x = await _connection.InsertAsync(student);
+
+        }
+        public async Task<IEnumerable<Student>> GetStudentsAsync()
+        {
+            return await _connection.Table<Student>()
+                        .OrderBy(s => s.Id).ToListAsync();
+        }
+
+        public async Task EditStudentAsync(Student student)
+        {
+            await _connection.UpdateAsync(student);
+        }
+
+        public async Task<Student> GetStudentByIdAsync(int id)
+        {
+            return await _connection.Table<Student>().FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task DeleteStudentAsync(Student student)
+        {
+            await _connection.DeleteAsync(student);
+        }
+
+        public async Task<IEnumerable<Student>> SortAscAsync()
+        {
+            var students = await _connection.Table<Student>()
+                           .OrderBy(s => s.Name).ToListAsync();
+            return students;
+        }
+        public async Task<IEnumerable<Student>> SortDescAsync()
+        {
+            var students = await _connection.Table<Student>()
+                           .OrderByDescending(s => s.Name).ToListAsync();
+            return students;
+        }
+
+        public async Task<Appointment> AddAppointmentAsync(Appointment appointment)
+        {
+            await _connection.InsertAsync(appointment);
+            return appointment;
+        }
+        public async Task<IEnumerable<Appointment>> GetAppointmentsAsync()
+        {
+            return await _connection.Table<Appointment>().ToListAsync();
+
+        }
+        public async Task<int> Count()
+        {
+            return await _connection.Table<Appointment>().CountAsync();
+        }
+        public async Task DeleteAppointmentAsync(Appointment appointment)
+        {
+            await _connection.Table<Appointment>()
+                    .DeleteAsync(a => a.StartDate == appointment.StartDate &&
+                    a.EndDate == appointment.EndDate);
+        }
+        public async Task<Appointment> EditAppointmentAsync(Appointment appointment)
+        {
+            var existingAppointment = await _connection.Table<Appointment>()
+                                .Where(a => a.StartDate == appointment.StartDate)
+                                .FirstOrDefaultAsync();
+
+            await _connection.UpdateAsync(appointment);
+
+            return appointment;
+        }
+
+        public async Task<int> GetAppointmentIdAsync(Appointment appointment)
+        {
+            var current_appointment = await _connection.Table<Appointment>()
+                .FirstOrDefaultAsync(a => a.StartDate == appointment.StartDate && a.EndDate == appointment.EndDate);
+
+            return current_appointment.Id;
+
+        }
+    }
+}
