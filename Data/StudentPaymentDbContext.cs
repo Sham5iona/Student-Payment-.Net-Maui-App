38916c776911844450097cf41e0f374681c34261
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-08-15 17:31:38 +0300

    Db Context
    Here I have added more methods and the last ones are for the analytics page. For example the count of Appointments and Students dbs and also to filter the appointments which aren't finished yet and the students which last amount is <= 20

--- Data/StudentPaymentDbContext.cs
+++ Data/StudentPaymentDbContext.cs
@@ -16,19 +16,200 @@ namespace StudentPaymentApp.Data
         {
             _connection = new SQLiteAsyncConnection(
                               Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
-            if (_connection != null)
+            if (_connection == null)
             {
                 return;
             }
 
             await _connection.CreateTableAsync<Student>();
+            await _connection.CreateTableAsync<Payment>();
             await _connection.CreateTableAsync<Appointment>();
         }
 
-        public async Task AddStudentAsync(Student student)
+        public async Task<Student> AddStudentAsync(Student student)
         {
             await _connection.InsertAsync(student);
+            return student;
 
+        }
+        public async Task<IEnumerable<Student>> GetStudentsAsync()
+        {
+
+            var students = await _connection.Table<Student>().ToListAsync();
+
+            var payments = await _connection.Table<Payment>().ToListAsync();
+
+            if (payments.Count > 0)
+            {
+                IList<Student> student_payments = new List<Student>();
+
+                foreach (var student in students)
+                {
+                    foreach (var payment in payments)
+                    {
+                        var student_payment = payments.FirstOrDefault(
+                                p => p.StudentId == student.Id);
+
+                        student.Payment = student_payment;
+
+                    }
+
+                    student_payments.Add(student);
+                }
+
+                return student_payments;
+
+            }
+
+            return students;
+
+            
+        }
+
+        public async Task EditStudentAsync(Student student)
+        {
+            await _connection.UpdateAsync(student);
+        }
+
+        public async Task<Payment> GetPaymentByIdAsync(int id)
+        {
+            return await _connection.Table<Payment>().FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<Student> GetStudentByIdAsync(int id)
+        {
+            return await _connection.Table<Student>().FirstOrDefaultAsync(s => s.Id == id);
+        }
+        
+        public async Task<Student> GetStudentPaymentByStudentIdAsync(int studentId)
+        {
+            var student = await GetStudentByIdAsync(studentId);
+
+            var payment = await _connection.Table<Payment>()
+                          .FirstOrDefaultAsync(p => p.StudentId == studentId);
+
+            student.Payment = payment;
+
+            return student;
+        }
+
+        public async Task DeleteStudentAsync(Student student)
+        {
+            await _connection.DeleteAsync(student);
+        }
+
+        public async Task AddPaymentAsync(Payment payment)
+        {
+            await _connection.InsertAsync(payment);
+        }
+
+        public async Task UpdatePaymentAsync(Payment payment)
+        {
+            await _connection.UpdateAsync(payment);
+        }
+
+        public async Task<IEnumerable<Appointment>> FilterAppointmentsBySearchTextAsync(string searchText)
+        {
+            var filteredAppointments = await _connection.Table<Appointment>()
+                                     .Where(a => 
+                                     a.Subject.ToLower().Contains(searchText.ToLower()))
+                                     .ToListAsync();
+
+            return filteredAppointments;
+        }
+
+        public async Task<IEnumerable<Student>> SortAscAsync()
+        {
+            var students = await _connection.Table<Student>().ToListAsync();
+
+            var payments = await _connection.Table<Payment>().ToListAsync();
+
+            if (payments.Count > 0)
+            {
+                IList<Student> student_payments = new List<Student>();
+
+                foreach (var student in students)
+                {
+                    foreach (var payment in payments)
+                    {
+                        var student_payment = payments.FirstOrDefault(
+                                p => p.StudentId == student.Id);
+
+                        student.Payment = student_payment;
+
+                    }
+
+                    student_payments.Add(student);
+                }
+
+
+                return student_payments.OrderBy(sp => sp.Name);
+
+            }
+
+            return students.OrderBy(s => s.Name);
+        }
+        public async Task<IEnumerable<Student>> SortDescAsync()
+        {
+            var students = await _connection.Table<Student>().ToListAsync();
+
+            var payments = await _connection.Table<Payment>().ToListAsync();
+
+            if (payments.Count > 0)
+            {
+                IList<Student> student_payments = new List<Student>();
+
+                foreach (var student in students)
+                {
+                    foreach (var payment in payments)
+                    {
+                        var student_payment = payments.FirstOrDefault(
+                                p => p.StudentId == student.Id);
+
+                        student.Payment = student_payment;
+
+                    }
+
+                    student_payments.Add(student);
+                }
+
+                return student_payments.OrderByDescending(sp => sp.Name); ;
+
+            }
+
+            return students;
+        }
+        public async Task<IEnumerable<Student>> FilterAsync(string search_string)
+        {
+            var students = await _connection.Table<Student>().ToListAsync();
+
+            var payments = await _connection.Table<Payment>().ToListAsync();
+
+            if (payments.Count > 0)
+            {
+                IList<Student> student_payments = new List<Student>();
+
+                foreach (var student in students)
+                {
+                    foreach (var payment in payments)
+                    {
+                        var student_payment = payments.FirstOrDefault(
+                                p => p.StudentId == student.Id);
+
+                        student.Payment = student_payment;
+
+                    }
+
+                    student_payments.Add(student);
+                }
+
+                return student_payments.Where(sp => sp.Name.ToLower()
+                           .Contains(search_string.ToLower()));
+
+            }
+
+            return students;
+            
         }
         public async Task<Appointment> AddAppointmentAsync(Appointment appointment)
         {
@@ -40,7 +221,7 @@ namespace StudentPaymentApp.Data
             return await _connection.Table<Appointment>().ToListAsync();
 
         }
-        public async Task<int> Count()
+        public async Task<int> AppointmentsCountAsync()
         {
             return await _connection.Table<Appointment>().CountAsync();
         }
@@ -64,10 +245,30 @@ namespace StudentPaymentApp.Data
         public async Task<int> GetAppointmentIdAsync(Appointment appointment)
         {
             var current_appointment = await _connection.Table<Appointment>()
-                .FirstOrDefaultAsync(a => a.StartDate == appointment.StartDate && a.EndDate == appointment.EndDate);
+                .FirstOrDefaultAsync(a => a.StartDate == appointment.StartDate &&
+                a.EndDate == appointment.EndDate && a.Subject == appointment.Subject
+                && a.Description == appointment.Description &&
+                a.Location == appointment.Location);
 
             return current_appointment.Id;
 
+        }
+        public async Task<int> StudentsCountAsync()
+        {
+            return await _connection.Table<Student>().CountAsync();
+        }
+        public async Task<IEnumerable<Appointment>> GetNotFinishedYetAppointmentsAsync()
+        {
+            return await _connection.Table<Appointment>()
+                    .Where(a => a.EndDate > DateTime.Now)
+                    .ToListAsync();
+        }
+        public async Task<IEnumerable<Student>> GetStudentsWithLastAmountUnderOrEqual20Async()
+        {
+            var students = await GetStudentsAsync();
+
+            return students.Where(s => s.Payment.LastAmount <= 20)
+                   .ToList();
         }
     }
 }
