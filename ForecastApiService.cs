0c03e480979f903e012e0d0361dd8a994360f913
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-08-18 16:20:37 +0300

    Service
    This class works with the HttpClient class and its role is to send the api request to the server and return the deserialized JSON response.

---
+++ ForecastApiService.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Net.Http.Json;
+
+namespace StudentPaymentApp.Model.Services
+{
+    public class ForecastApiService
+    {
+        private readonly HttpClient _httpClient;
+        public ForecastApiService()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(Constants.API_BASE_URL);
+        }
+
+        public async Task<ForecastAPIResponse> GetForecastInformationAsync(string latitude, string longitude)
+        {
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            {
+                return null;
+            }
+             return await _httpClient.GetFromJsonAsync<ForecastAPIResponse>
+                    ($"current?access_key={Constants.API_KEY}&query={latitude}, {longitude}");
+        }
+    }
+}
