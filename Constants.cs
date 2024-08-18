6c3721e86a367d13f1a0853321716311c3a326c6
Sham5iona <107301002+Sham5iona@users.noreply.github.com>
2024-08-18 16:19:17 +0300

    Models
    Here are the new models for the forecast functionality. The Constants class is a static one which holds the API_BASE_ADDRESS and the API_KEY.
    The other file holds the classes which will hold the deserialized JSON data from the server.

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentPaymentApp.Model
+{
+    internal static class Constants
+    {
+        public const string API_KEY = "8ff1c5ddb9504b8cc33bd1d32b9c51f8";
+        public const string API_BASE_URL = "https://api.weatherstack.com";
+    }
+}
---
+++ ForecastAPIResponse.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StudentPaymentApp.Model
+{
+    public class ForecastAPIResponse
+    {
+        [JsonPropertyName("location")]
+        public ForecastAPIResponseLocation Location { get; set; }
+
+        [JsonPropertyName("current")]
+        public ForecastAPIResponseCurrent Current { get; set; }
+    }
+
+    public class ForecastAPIResponseLocation
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+        public string localtime { get; set; }
+
+    }
+
+    public class ForecastAPIResponseCurrent
+    {
+        public double temperature { get; set; }
+        public string[] weather_icons { get; set; }
+        public string[] weather_descriptions { get; set; }
+        public double wind_speed { get; set; }
+        public double humidity { get; set; }
+        public double cloudcover { get; set; }
+        public double feelslike { get; set; }
+        public double uv_index { get; set; }
+        public double visibility { get; set; }
+        public string is_day { get; set; }
+    }
+}
